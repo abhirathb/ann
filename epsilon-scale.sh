038d86507743ad70f67263c6b9f22691599aff73
Abhirath Batra <abhs.94@gmail.com>
2017-08-23 13:42:11 +0530

    all corrected

--- epsilon-scale.sh
+++ epsilon-scale.sh
@@ -1,3 +1,6 @@
+cd result_22-8/eps-scale/
+rm diff_1000_100 pot_1000_100 ham_1000_100 diff-k_1000_100 kin_1000_100 ratio_1000_100 ratio-k_1000_100
+cd ../../
 for i in 0.1 0.01 0.001 0.0001 0.00001
 do
     python ann-fixed.py 1000 100 $i > result_22-8/eps-scale/out_1000_100_$i
